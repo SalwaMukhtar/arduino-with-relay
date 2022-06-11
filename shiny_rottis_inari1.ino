dde2df7eb31039d79dcfbffd5cd1b53d50ead8eb
Salwa Mukhtar <102853846+SalwaMukhtar@users.noreply.github.com>
2022-06-11 11:58:32 +0500

    Add files via upload

---
+++ shiny_rottis_inari1.ino
@@ -0,0 +1,14 @@
+#define relay 2
+
+void setup()
+{
+  pinMode(relay, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop(){
+  digitalWrite(relay, HIGH);
+  delay(1000); 
+  digitalWrite(relay, LOW);
+  delay(1000);
+}
